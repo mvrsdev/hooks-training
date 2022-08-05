5e6d36836fd9c54c855dc65e161a1555e0d9afad
Thiago Dias <thiagodbrt@gmail.com>
2022-08-04 23:13:41 -0300

    Add new messages in coming soon page

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -22,25 +22,58 @@ const titleMessages = [
   'Cool your jets',
 ];
 
-const getRandomMessage = () => {
-  return titleMessages[Math.floor(Math.random() * titleMessages.length)];
+const descriptionMessages = [
+  [
+    'This exercise will be ready soon, little padawan.',
+    'Go home, take a rest and come back later.',
+  ],
+  [
+    "Go take a shower, listen to good music and relax.",
+    'Next lessons will come soon.',
+  ],
+  [
+    "Weren't you a developer in a previous life?",
+    'The next lesson will be ready soon.',
+  ],
+  [
+    'Wow! I bet you are the fastest man alive! ⚡️',
+    'This lesson will be live in a flash.',
+  ],
+];
+
+const buttonMessages = [
+  "Okay! I'll be back",
+  "Fine! I'm leaving",
+  'Okay! Bye',
+  'All right',
+  'No problem. Just going',
+];
+
+const getRandomMessage = (messages) => {
+  return messages[Math.floor(Math.random() * messages.length)];
 };
 
-const ComingSoon = () => (
-  <div style={style}>
-    <img src="/images/coming-soon.svg" alt="Coming soon" style={imageStyle} />
-    <div>
-      <h1 style={{ marginLeft: -32, zIndex: 0 }}>{getRandomMessage()}</h1>
-      <p style={{ paddingLeft: 16, fontSize: 18 }}>
-        This exercise will be ready soon, little padawan.
-        <br />
-        Go home, take a rest and come back later.
-      </p>
-      <Link to="/">
-        <button>Okay! I'm going back</button>
-      </Link>
+const ComingSoon = () => {
+  const title = getRandomMessage(titleMessages);
+  const description = getRandomMessage(descriptionMessages);
+  const buttonMessage = getRandomMessage(buttonMessages);
+
+  return (
+    <div style={style}>
+      <img src="/images/coming-soon.svg" alt="Coming soon" style={imageStyle} />
+      <div>
+        <h1 style={{ marginLeft: -32, zIndex: 0 }}>{title}</h1>
+        <p style={{ paddingLeft: 16, fontSize: 18 }}>
+          {description[0]}
+          <br />
+          {description[1]}
+        </p>
+        <Link to="/">
+          <button>{buttonMessage}</button>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ComingSoon;
